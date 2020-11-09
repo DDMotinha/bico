bae68a0d9500300628ca4f41273385adb8e303d3
David Mariano Gimenez Mota <davidmariangmota@gmail.com>
2020-11-09 16:47:51 -0300

    Está sendo realizado toda a aplicação em Bootstrap

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="/src/style/home.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+    <title>Bico online</title>
+</head>
+<body>
+    <div class="container-xl navbar fixed-top navigation">
+        <div class="col-3">
+            <div class="col-3">
+                <a class="navbar-brand" href="/">
+                    <img src="./src/image/b-logo.png" alt="b-logo">
+                </a>
+            </div>
+        </div>
+        <div class="col-6 navbar">
+            <div class="col-3">
+                <a href="http://" class="btn text-warning">Vagas</a>
+            </div>
+            <div class="col-3">
+                <a href="http://" class="btn text-warning">Anuncie</a>
+            </div>
+            <div class="col-3">
+                <a href="http://" class="btn text-warning">Entenda</a>
+            </div>
+            <div class="col-3">
+                <a href="http://" class="btn text-warning">Sobre</a>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="text-center">
+                <button onclick="" class="btn btn-outline-warning">Entre aqui!</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
