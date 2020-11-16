6e8bdc1c07d431438fdf97a71d449de73b1119fe
David Mariano Gimenez Mota <davidmariangmota@gmail.com>
2020-11-16 15:29:15 -0300

    continuação da montagem

--- index.php
+++ index.php
@@ -1,147 +1,122 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
+
     <!-- Tags metas funcionais -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="/src/style/home.css">
+    <link rel="stylesheet" href="/src/style/style.css">
     <!-- CSS bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     
     <!-- Script bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    
+    <script src="./src/controller/script.js"></script>
     <title>Bico Online</title>
+
 </head>
+
 <body>
-    <nav class="container-fluid navbar fixed-top navigation bg-light border border-warning rounded-bottom rounded border-top-0">
-        <div class="col-3">
+
+    <nav class="container-fluid navbar fixed-top navigation bg-light border border-warning rounded-bottom rounded border-top-0"><!--Abre nav-->
+        
+        <div class="col-3"><!--Abre B-Logo-->
             <div class="col-3">
                 <a class="navbar-brand" href="/">
                     <img src="./src/image/b-logo.png" alt="b-logo" class="b-logo">
                 </a>
             </div>
         </div><!--Fecha B-Logo-->
-        <div class="col-6 navbar">
-            <div class="col-3">
-                <a href="http://" class="btn text-warning">Vagas</a>
+
+        <div class="col-6 navbar"><!--Abre Elementos de pesquisa-->
+
+            <div class="col-3"><!--Abre Vagas-->
+                <a href="/vagas" class="btn text-warning">Vagas</a>
             </div><!--Fecha Vagas-->
-            <div class="col-3">
-                <a href="http://" class="btn text-warning">Anuncie</a>
+
+            <div class="col-3"><!--Abre Anuncie-->
+                <a href="/anuncie" class="btn text-warning">Anuncie</a>
             </div><!--Fecha Anuncie-->
-            <div class="col-3">
-                <a href="http://" class="btn text-warning">Entenda</a>
+
+            <div class="col-3"><!--Abre entenda-->
+                <a href="/entenda" class="btn text-warning">Entenda</a>
             </div><!--Fecha Entenda-->
-            <div class="col-3">
-                <a href="http://" class="btn text-warning">Sobre</a>
+
+            <div class="col-3"><!--Abre Sobre-->
+                <a href="/sobre" class="btn text-warning">Sobre</a>
             </div><!--Fecha Sobre-->
+
         </div><!--Fecha Elementos de pesquisa-->
-        <div class="col-3">
+
+        <div class="col-3"><!-- Abre Clique de login -->
             <div class="text-center">
-                <button onclick="" class="btn btn-outline-success">Entre aqui!</button>
+                <button onclick="redirecionar()" class="btn btn-outline-success">Entre aqui!</button>
             </div>
-        </div><!--Fecha Clique de login-->
+        </div><!-- Fecha Clique de login -->
+
     </nav><!-- Fecha barra de navegação -->
-    <main class="container-fluid mt-5 bg-warning m-auto">
-        <div id="carouselExampleFade" class=" shadow-sm carousel slide container-lg border-warning rounded-bottom rounded-lg p-1 bg-light border mb-5 rounded" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="./src/image/b-1-carousel.png" class="d-block w-100" alt="b-1-carousel">
-                </div>
-                <div class="carousel-item">
-                    <img src="./src/image/b-2-carousel.png" class="d-block w-100" alt="b-2-carousel">
-                </div>
-                <div class="carousel-item">
-                    <img src="./src/image/b-3-carousel.png" class="d-block w-100" alt="b-3-carousel">
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Anterior</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Próximo</span>
-            </a>
-        </div><!-- Fecha Carousel -->
-        <div class="row container-xl bg-light text-center rounded mx-auto bg-white rounded-bottom navbar border border-info">
+
+    <main class="container-fluid mt-5 bg-warning p-0">
+
+        <div class="row container bg-warning text-center rounded mx-auto bg-white p-5 m-5"><!--ABRE SISTEMA DE BUSCAS-->
             <div class="row">
                 <div class="text-center row">
                     <div class="input-group mb-3 col">
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" id="b-pesquisa">
                     </div>
                     <div class="col">
                         <button class="btn btn-primary">Procurar bicos</button>
                     </div>
                 </div>
             </div>
-            <div class="col">
-                <div class="text-center">
+        </div> <!--Fecha o sistema de busca-->
 
-                </div>
-            </div>
-            <div class="col">
-                <div class="text-center">
-
-                </div>
-            </div>
-            <div class="col">
-                <div class="text-center">
-
-                </div>
-            </div>
-            <div class="col">
-                <div class="text-center">
-
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid bg-warning pt-5 pb-3">
-            <div class="row mx-md-n5">
-                <div class="col px-md-5 mb-5 text-center">
-                    <div class="border border-info bg-light rounded rounded-bottom pb-5">
-                        <img src="./src/image/b-1-banner.svg" alt="b-1-banner" srcset="" id="b-1-banner" class="mb-3 b-banner">
-                        <label for="b-1-banner">
-                            <h6>
-                                Trace suas metas.
-                            </h6>
-                        </label>
-                    </div>
-                </div>
-                <div class="col px-md-5 mb-5 text-center">
-                    <div class="border border-info bg-light rounded rounded-bottom pb-5">
-                        <img src="./src/image/b-2-banner.svg" alt="b-2-banner" srcset="" id="b-2-banner" class="mb-3 b-banner">
-                        <label for="b-2-banner">
-                            <h6>
-                                Conquiste seus objetivos.
-                            </h6>
-                        </label>
-                    </div>
-                </div>
-                <div class="col px-md-5 mb-5 text-center">
-                    <div class="border border-info bg-light rounded rounded-bottom pb-5">
-                        <img src="./src/image/b-3-banner.svg" alt="b-3-banner" srcset="" id="b-3-banner" class="mb-3 b-banner">
-                        <label for="b-3-banner">
-                            <h6>
-                                Construa o seu horário.
-                            </h6>
-                        </label>
-                    </div>
-                </div>
-                <div class="col px-md-5 mb-5 text-center">
-                    <div class="border border-info bg-light rounded rounded-bottom pb-5">
-                        <img src="./src/image/b-4-banner.svg" alt="b-4-banner" srcset="" id="b-4-banner" class="mb-3 b-banner">
-                        <label for="b-4-banner">
-                            <h6>
-                                Brilhe em suas atividade.
-                            </h6>
-                        </label>
+        <div class="col bg-top text-center container-fluid bg-light rounded-lg"> <!---Abre os banners de engajamento-->
+            <div class="row p-3 rounded">
+                <div class="col text-justify m-1">
+                    <div class="col p-paraph ">
+                        <div class="col mb-5 mt-5 text-center p-text-tittle">
+                            Encontre a <strong>independência</strong> financeira!
+                        </div>
+                        <div class="row p-text-body ">
+                            <div class="col rounded-lg p-3 m-5 text-center rounded rounded-lg p-1 mb-5 shadow-sm">
+                                <img src="./src/image/b-1-banner.svg" alt="p-bn-promo" class="p-bn-banner col p-1 mb-5 ">
+                                <div class="mb-5">
+                                    <div class="row-3">
+                                        Consquiste o <strong>seu</strong> mercado!
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col rounded-lg p-3 m-5 text-center rounded rounded-lg p-1 mb-5 shadow-sm">
+                                <img src="./src/image/b-4-banner.svg" alt="p-bn-promo" class="p-bn-banner col p-1 mb-5 ">
+                                <div class="mb-5">
+                                    <div class="row-3">
+                                        Construa sua <strong>independência</strong>!
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col rounded-lg p-3 m-5 text-center rounded rounded-lg p-1 mb-5 shadow-sm">
+                                <img src="./src/image/b-3-banner.svg" alt="p-bn-promo" class="p-bn-banner col mb-5 ">
+                                <div class="mb-5">
+                                    <div class="row-3">
+                                        <div class="col p-text-tittle mb-3">Quer saber mais?</div>
+                                        <button class="btn btn-outline-success" onclick="redirecionar()">
+                                            Clique aqui!
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div><!-- Fecha Banner de engajamento -->
+        </div> <!---Fecha o banners de engajamento-->
+
     </main><!-- Fecha o main -->
+
 </body>
 </html>
